2cb6b3ee02c0eae133855cdbc2ac575697c601c8
Ryan <ryankilbane92@hotmail.com>
2021-01-15 03:29:13 +0000

    Added Lexer struct and some methods

--- src/lexer/lexer.rs
+++ src/lexer/lexer.rs
@@ -1,3 +1,28 @@
 mod lexer{
-    
+    struct Lexer<'a>{
+        input: Option<&'a str>,
+        position: Option<u32>,
+        read_position: Option<u32>,
+        chr: Option<char>
+    }
+
+    impl<'a> Lexer<'a>{
+        pub fn new(self, input: &'a str) -> Self{
+            Lexer{
+                input: Some(input),
+                position: None,
+                read_position: None,
+                chr: None
+            }
+        }
+
+        fn read_char(self){
+        }
+    }
+
+    #[cfg(test)]
+    mod test{
+        use super::*;
+
+    }
 }
