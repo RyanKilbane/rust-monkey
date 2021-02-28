ff45431dd6ad703d95fa7a8a25ae05134e750c52
Ryan <ryankilbane92@hotmail.com>
2021-02-28 12:48:17 +0000

    Found out how to compose traits. Added beginings of AST struct and methods

--- src/parser/ast.rs
+++ src/parser/ast.rs
@@ -3,13 +3,23 @@ pub mod ast{
         fn token_literal(&mut self);
     }
 
-    trait Statement{
-        fn token_literal(&mut self);
+    trait Statement: Node{
         fn statement_node(&mut self);
     }
 
-    trait Expression{
-        fn token_literal(&mut self);
+    trait Expression: Node{
         fn expression_node(&mut self);
+    }
+
+    struct AST{
+
+    }
+
+    impl AST{
+        pub fn new(&mut self) -> Self{
+            AST{
+
+            }
+        }
     }
 }
