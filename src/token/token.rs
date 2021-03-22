5b4d708a314f4e66253a5895b52771b558313087
Ryan <ryankilbane92@hotmail.com>
2021-03-22 20:22:23 +0000

    Added return statement to keywords

--- src/token/token.rs
+++ src/token/token.rs
@@ -38,11 +38,13 @@ pub mod token{
     // Keywords
     pub const FUNCTION: &str = "FUNCTION";
     pub const LET: &str = "LET";
+    pub const RETURN: &str = "RETURN";
 
     pub fn ident_lookup(lookup: &str) -> String{
         let mut keyword_map: HashMap<&str, &str> = HashMap::new();
         keyword_map.insert("fn", FUNCTION);
         keyword_map.insert("let", LET);
+        keyword_map.insert("return", RETURN);
         if keyword_map.contains_key(lookup){
             keyword_map.get(lookup).unwrap().to_owned().to_string()
         }
